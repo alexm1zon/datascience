27e1f59711d59306744b22009a840828fe0234e0
Alex Mizon <alexmizon@Alexs-MacBook-Pro.local>
2018-04-03 20:37:53 -0400

    update excel creation

--- Data Staging/stagingScript.py
+++ Data Staging/stagingScript.py
@@ -49,32 +49,38 @@ for row in reader:
         locationKeys = Location.getLocationKeys(row["PLACE"])
         index = 0
         while index < len(locationKeys):
-            startDateKey.append(startDateKey_value)
-            endDateKey.append(endDateKey_value)
-            locationKey.append(locationKeys[index])
-            disasterKey.append(disasterKey_value)
-            descriptionKey.append(descriptionKey_value)
-            costKey.append(costKey_value)
-            fatalities.append(fatalities_value)
-            injured.append(injured_value)
-            evacuated.append(evacuated_value)
+            startDateKey = startDateKey_value
+            endDateKey = endDateKey_value
+            locationKey = locationKeys[index]
+            disasterKey= disasterKey_value
+            descriptionKey = descriptionKey_value
+            costKey = costKey_value
+            fatalities = fatalities_value
+            injured = injured_value
+            evacuated = evacuated_value
             index = index + 1
 
-        testModeCount = testModeCount+1
-        if (testMode==True and testModeCount==50):
-            break;
-df.insert(loc=0, column='startDateKey', value=pd.Series(startDateKey))
-df.insert(loc=1, column='endDateKey', value=pd.Series(startDateKey))
-df.insert(loc=2, column='locationKey', value=pd.Series(locationKey))
-df.insert(loc=3, column='disasterKey', value=pd.Series(disasterKey))
-df.insert(loc=4, column='descriptionKey', value=pd.Series(descriptionKey))
-df.insert(loc=5, column='costKey', value=pd.Series(costKey))
-df.insert(loc=6, column='fatalities', value=pd.Series(fatalities))
-df.insert(loc=7, column='injured', value=pd.Series(injured))
-df.insert(loc=8, column='evacuated', value=pd.Series(evacuated))
-
-df.to_csv(filenameFACT, encoding='utf-8', index=False)
-print(Location.get_success_count() + '=success')
-print(Location.get_problems_count() + '=problems')
-print(Location.get_total_count() + '=total')
+        # testModeCount = testModeCount+1
+        # if (testMode==True and testModeCount==50):
+        #     break;
+            
+        with open(filenameFACT, 'ab') as ff:
+            writer = csv.writer(ff)
+            writer.writerow([startDateKey,endDateKey,locationKey,disasterKey,descriptionKey,
+                             costKey,fatalities,injured,evacuated])
+            
+# df.insert(loc=0, column='startDateKey', value=pd.Series(startDateKey))
+# df.insert(loc=1, column='endDateKey', value=pd.Series(startDateKey))
+# df.insert(loc=2, column='locationKey', value=pd.Series(locationKey))
+# df.insert(loc=3, column='disasterKey', value=pd.Series(disasterKey))
+# df.insert(loc=4, column='descriptionKey', value=pd.Series(descriptionKey))
+# df.insert(loc=5, column='costKey', value=pd.Series(costKey))
+# df.insert(loc=6, column='fatalities', value=pd.Series(fatalities))
+# df.insert(loc=7, column='injured', value=pd.Series(injured))
+# df.insert(loc=8, column='evacuated', value=pd.Series(evacuated))
+# 
+# df.to_csv(filenameFACT, encoding='utf-8', index=False)
+# print(Location.get_success_count() + '=success')
+# print(Location.get_problems_count() + '=problems')
+# print(Location.get_total_count() + '=total')
 
